f15e6b7c7bf8ef2ec1b0649c420b1b36b5bff472
csingleton <codysingleton@gmail.com>
2013-11-26 03:17:34 -0600

    added missing header

---
+++ Headers/BCBeaconDataSource.h
@@ -0,0 +1,23 @@
+//
+//  BCBeaconDataSource.h
+//  BlueCatsSDK
+//
+//  Created by Cody Singleton on 11/5/13.
+//  Copyright (c) 2013 BlueCats. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol BCBeaconDataSource <NSObject>
+
+@required
+
+- (NSDate *)cachedBeaconsAt;
+- (NSArray *)cachedBeacons;
+- (BOOL)hasBeaconsCacheExpired;
+
+- (void)loadBeaconsWithSuccess:(void (^)(NSArray *))success
+                       failure:(void (^)(NSError *))failure
+                  preferCached:(BOOL)preferCached;
+
+@end
